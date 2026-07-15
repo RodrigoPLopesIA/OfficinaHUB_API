a77780d9c19be2ce16a11667a2b3188206d603bb
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2026-07-14 22:29:44 -0300

    feat: create project

---
+++ src/main/java/com/rodrigoplopesai/officinahub_api/OfficinahubApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigoplopesai.officinahub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficinahubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficinahubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigoplopesai/officinahub_api/OfficinahubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoplopesai.officinahub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficinahubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
