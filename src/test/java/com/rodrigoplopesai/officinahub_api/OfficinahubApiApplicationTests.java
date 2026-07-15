4ad92fb6841ba323edd90cea41ba9deeb0a82e21
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2026-07-14 23:02:39 -0300

    feat: remove flyway from tests

--- src/test/java/com/rodrigoplopesai/officinahub_api/OfficinahubApiApplicationTests.java
+++ src/test/java/com/rodrigoplopesai/officinahub_api/OfficinahubApiApplicationTests.java
@@ -3,7 +3,9 @@ package com.rodrigoplopesai.officinahub_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.flyway.enabled=false"
+})
 class OfficinahubApiApplicationTests {
 
 	@Test
